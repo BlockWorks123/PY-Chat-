c82bb3593dace8745ccffb75022166e687642b55
BlockWorks123 <BlockWorks123@outlook.com>
2021-10-29 11:13:15 +0100

    4.0.1

--- PyChat Client.py
+++ PyChat Client.py
@@ -5,7 +5,6 @@ import random
 from datetime import datetime
 import os
 import socket
-from socket import socket, AF_INET, SOCK_STREAM
 import threading
 #Welcome Message
 print("-----------------------------------")
