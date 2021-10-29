937a62e97ac03119eaf348050fbd35df1f005662
BlockWorks123 <BlockWorks123@outlook.com>
2021-10-29 10:13:15 +0100

    port change

--- PyChat Client.py
+++ PyChat Client.py
@@ -16,7 +16,7 @@ print("-----------------------------------")
 #Socket Connection
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "192.168.0.33"
-port = 80
+port = 12345
 my_socket.connect((host, port))
 
 #Nickname Input
