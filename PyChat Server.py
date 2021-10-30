57a4260fadc356565c5a7e7692b081924426387b
BlockWorks123 <BlockWorks123@outlook.com>
2021-10-30 12:50:14 +0100

    v1.1.6

--- PyChat Server.py
+++ PyChat Server.py
@@ -25,39 +25,64 @@ PORT = 12345
 broadcast_list = []
 my_socket.bind((ADDRESS, PORT))
 
+while True:
+    command = input("]")
+    if command == "/help":
+        print("/help -- Shows all commands")
+        print("/time -- Shows current time")
+        print("/clear -- Clears terminal ")
+        print("/shutdown -- Shutsdown the server")
+    if command == "/shutdown":
+        exit()
+    if command == "/time":
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print("Time :",current_time)
+    if command == "/clear":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("---------------------------------")
+        print("----Welcome To PY:Chat Server----")
+        print("----Developed By BlockWorks123---")
+        print("---------------------------------")
+    else:
+        for client in broadcast_list:
+            try:
+                client.send(command.encode())
+            except:
+                broadcast_list.remove(client)
+                print("Client removed",client)
+    #Socket listening for message
+    def accept_loop():
+        while True:
+            my_socket.listen()
+            client, client_address = my_socket.accept()
+            broadcast_list.append(client)
+            start_listenning_thread(client)
 
-#Socket listening for message
-def accept_loop():
-    while True:
-        my_socket.listen()
-        client, client_address = my_socket.accept()
-        broadcast_list.append(client)
-        start_listenning_thread(client)
+    #Socket listening for nickname     
+    def start_listenning_thread(client):
+        client_thread = threading.Thread(
+                target=listen_thread,
+                args=(client,) #the list of argument for the function
+            )
+        client_thread.start()
 
-#Socket listening for nickname     
-def start_listenning_thread(client):
-    client_thread = threading.Thread(
-            target=listen_thread,
-            args=(client,) #the list of argument for the function
-        )
-    client_thread.start()
-
-#Client message receiver and broadcaster
-def listen_thread(client):
-    while True:
-        message = client.recv(1024).decode()
-        if message:
-            print("Client Message>",message)
-            broadcast(message)
-        else:
-            print(f"client has been disconnected : {client}")
-            return
-#Server "client diconnect" message
-def broadcast(message):
-    for client in broadcast_list:
-        try:
-            client.send(message.encode())
-        except:
-            broadcast_list.remove(client)
-            print("Client removed",client)
-accept_loop()      
+    #Client message receiver and broadcaster
+    def listen_thread(client):
+        while True:
+            message = client.recv(1024).decode()
+            if message:
+                print("Client Message>",message)
+                broadcast(message)
+            else:
+                print(f"client has been disconnected : {client}")
+                return
+    #Server "client diconnect" message
+    def broadcast(message):
+        for client in broadcast_list:
+            try:
+                client.send(message.encode())
+            except:
+                broadcast_list.remove(client)
+                print("Client removed",client)
+    accept_loop()      
