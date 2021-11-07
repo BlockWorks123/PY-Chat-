44a58d7c3f40077e1dca97b4c024d9eb210b06e7
BlockWorks123 <BlockWorks123@outlook.com>
2021-11-07 13:38:41 +0000

    v1.1

--- PYChat Server Beta.py
+++ PYChat Server Beta.py
@@ -1,1 +1,27 @@
+
+#Libary Define
 import socket
+import threading
+import os
+from datetime import datetime 
+from math import trunc
+
+#Terminal Size
+cmd = 'mode 37,50'
+os.system(cmd)
+os.system("title PY:Chat 4.0")
+
+#Welcome message
+print("-----------------------------------")
+print("-----Welcome To PY:Chat Server-----")
+print("-----Developed By BlockWorks123----")
+print("-----------------------------------")
+
+#Command Console
+while True:
+    command = input("]")
+    if command == "/help":
+        print("/help -- Shows all commands")
+        print("/time -- Shows current time")
+        print("/clear -- Clears terminal ")
+        print("/shutdown -- Shutsdown the server")
